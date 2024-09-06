03284db6cdf097887b5a22aa77d8116121f36e67
JeffJ-L <e2396650@cmaisonneuve.qc.ca>
2024-09-06 12:32:25 -0400

    progression

--- wp-content/themes/twentytwentyone-child/front-page.php
+++ wp-content/themes/twentytwentyone-child/front-page.php
@@ -3,44 +3,17 @@
 get_header();
 
 
-$args =array(
-    'posts_per_page'    => -1,
-    'post_type'     => 'article',
-);
 
 
-$the_query = new WP_Query( $args );
 
-// var_dump($the_query);
+$accueil_introduction = get_field('accueil_introduction');
+// var_dump($accueil_introduction);
+?>
 
-if( $the_query->have_posts() ): ?>
-    <section>
-    <?php while( $the_query->have_posts() ) :
-         $the_query->the_post();
-        $champs = get_fields();
-        var_dump($champs);
-        die();
-
-        $produit_description = $champs['produit_description'];
-        $produit_image = $champs['produit_image']['url'];
-        $produit_prix = $champs['produit_prix'];
-
-    ?>
+<div class="accueil-intro">
+    <?php echo $accueil_introduction; ?>
+</div>
 
 
-        <article>
-            <a href="<?php the_permalink(); ?>">   
-            <?php if($produit_image): ?> 
-                <img src="<?php echo $produit_image; ?>" />
-            <?php endif; ?>
-                <h3><?php the_title(); ?></h3>
-            </a>
-        </article>    
-    <?php endwhile; ?>
-    </section>
-<?php endif; ?>
-
-<?php wp_reset_query();   // Restore global post data stomped by the_post().
-
-
+<?php
 get_footer();
---
+++ wp-content/themes/twentytwentyone-child/template-parts/a-propos.php
@@ -0,0 +1,11 @@
+<?php
+?>
+
+<div>
+    <?php get_field('a_propos_editeur'); ?>
+</div>
+
+<?php
+var_dump(get_field('a_propos_editeur'));
+
+get_footer();
